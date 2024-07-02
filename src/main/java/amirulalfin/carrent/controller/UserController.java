37a7098bfd3555f0084c1993611ae2bdd43d5007
Muhammad Amirul <mirulid@gmail.com>
2024-07-02 09:55:37 +0700

    apsih

--- src/main/java/amirulalfin/carrent/controller/UserController.java
+++ src/main/java/amirulalfin/carrent/controller/UserController.java
@@ -5,10 +5,9 @@ import amirulalfin.carrent.model.User;
 import amirulalfin.carrent.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
@@ -20,5 +19,31 @@ public class UserController {
     public User create(@RequestBody User request) {
         return userService.create(request);
 
+    }
+    @GetMapping
+    public List<User> getAll() {
+        return userService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public User getByid(@PathVariable Integer id){
+        return userService.getById(id);
+    }
+
+    @PutMapping
+    public User update(@RequestBody User request) {
+        return userService.update(request);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Integer id) {
+        userService.delete(id);
+    }
+
+    @PutMapping("/{id}")
+    public User topUp(@PathVariable Integer id,@PathVariable Integer nominal) {
+        User user = userService.getById(id);
+        user.setBalance(user.getBalance()+nominal);
+       return userService.topUp(id, user.getBalance());
     }
 }
