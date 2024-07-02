4e7779627b890bccf2d5f1902280df566a72b0c0
Alfin Kurniawan Aryanto <at.alfinkurniawan@gmail.com>
2024-07-02 09:29:43 +0700

    feat: add all DTO

---
+++ src/main/java/amirulalfin/carrent/utils/DTO/BrandDTO.java
@@ -0,0 +1,12 @@
+package amirulalfin.carrent.utils.DTO;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class BrandDTO {
+    private String name;
+}
