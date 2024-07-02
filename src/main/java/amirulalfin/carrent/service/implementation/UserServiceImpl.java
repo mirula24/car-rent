732a43a8388654fe9bbabf4775224459becbb009
Muhammad Amirul <mirulid@gmail.com>
2024-07-02 09:34:07 +0700

    users

---
+++ src/main/java/amirulalfin/carrent/service/implementation/UserServiceImpl.java
@@ -0,0 +1,45 @@
+package amirulalfin.carrent.service.implementation;
+
+import amirulalfin.carrent.model.User;
+import amirulalfin.carrent.repository.UserRepository;
+import amirulalfin.carrent.service.UserService;
+
+import java.util.List;
+
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+    @Override
+    public User create(User Request) {
+        return userRepository.save(Request);
+    }
+
+    @Override
+    public List<User> getAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User getById(Integer id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public User update(User Request) {
+        User user =this.getById(Request.getId());
+        user.setName(Request.getName());
+        user.setBalance(Request.getBalance());
+        return userRepository.save(user);
+    }
+
+    @Override
+    public void delete(Integer id) {
+        userRepository.deleteById(id);
+    }
+
+    @Override
+    public User topUp(Integer id, Integer nominal) {
+        User user = this.getById(id);
+        user.setBalance(nominal);
+        return userRepository.save(user);
+    }
+}
